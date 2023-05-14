a536f8308e01233780e4b7f2d0852926cb8475d7
odd <1161465+oddsun@users.noreply.github.com>
2023-05-14 01:27:17 -0400

    + pre-commit hooks, unit tests, GitHub actions

--- label_data/main.py
+++ label_data/main.py
@@ -2,14 +2,12 @@ import os
 from pathlib import Path
 
 from dotenv import load_dotenv
-from fastapi import FastAPI, Request, Depends, HTTPException, Form
-from fastapi import File, UploadFile
-from fastapi.responses import FileResponse
-from fastapi.responses import RedirectResponse
+from fastapi import Depends, FastAPI, File, Form, HTTPException, Request, UploadFile
+from fastapi.responses import FileResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from more_itertools import batched
 from sqlalchemy import create_engine, select
-from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.orm import Session, sessionmaker
 from starlette.responses import StreamingResponse
 
 from label_data.models import Headline
@@ -18,7 +16,7 @@ from label_data.models import Headline
 
 load_dotenv()
 
-DATABASE_URL = os.environ.get("DATABASE_URL")
+DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///headlines.db")
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 BASE_DIR = Path(__file__).parent
@@ -31,6 +29,7 @@ templates = Jinja2Templates(directory=BASE_DIR / "templates")
 
 # Dependency
 def get_db():
+    """get db session"""
     db = SessionLocal()
     try:
         yield db
@@ -40,51 +39,65 @@ def get_db():
 
 @app.get("/")
 async def read_headline(request: Request, db: Session = Depends(get_db)):
+    """show page for classification if any else show finished page"""
     headline = db.scalars(
-        select(Headline).where(Headline.sentiment == None).limit(1)
+        select(Headline).where(Headline.sentiment == None).limit(1)  # noqa
     ).first()
 
     if headline:
-        return templates.TemplateResponse("index.html",
-                                          {"request": request, "headline": headline})
+        return templates.TemplateResponse(
+            "index.html", {"request": request, "headline": headline}
+        )
     else:
         first_ten_records = db.scalars(select(Headline).limit(10)).all()
-        return templates.TemplateResponse("finished.html",
-                                          {"request": request,
-                                           "headlines": first_ten_records})
+        return templates.TemplateResponse(
+            "finished.html", {"request": request, "headlines": first_ten_records}
+        )
 
 
 @app.post("/classify")
-async def rate_headline(sentiment: str = Form(...), headline_id: int = Form(...),
-                        category: str = Form(...),
-                        db: Session = Depends(get_db)):
-    headline = db.scalars(
-        select(Headline).where(Headline.id == headline_id).limit(1)
-    ).first()
+async def rate_headline(
+    sentiment: str = Form(...),
+    headline_id: int = Form(...),
+    category: str = Form(...),
+    db: Session = Depends(get_db),
+):
+    """record headline classification"""
+    headline = await select_headline_by_id(db, headline_id)
     headline.sentiment = sentiment
     headline.category = category
     db.commit()
     return RedirectResponse("/", 303)
 
 
-@app.post("/undo/{headline_id}")
-async def undo_headline(headline_id: int, db: Session = Depends(get_db)):
+async def select_headline_by_id(db: Session, headline_id: int):
+    """select headline from db by id"""
     headline = db.scalars(
         select(Headline).where(Headline.id == headline_id).limit(1)
     ).first()
+    if not headline:
+        raise ValueError(f"headline with {headline_id=} not found")
+    return headline
+
+
+@app.post("/undo/{headline_id}")
+async def undo_classification(headline_id: int, db: Session = Depends(get_db)):
+    """undo classification"""
+    headline = await select_headline_by_id(db, headline_id)
     headline.sentiment = None
+    headline.category = None
     db.commit()
     return RedirectResponse("/", 303)
 
 
 def csv_reader(content: str):
-    """ basic csv reader
+    """basic csv reader
 
     This does not handle escaped commas within quotes. The user needs to ensure the
     commas are correctly escaped as `&comma;` (html entity).
     """
     for row in content.splitlines()[1:]:
-        items = row.split(',')
+        items = row.split(",")
         if len(items) <= 1:
             continue
         index, identifier, headline, name = items
@@ -93,45 +106,61 @@ def csv_reader(content: str):
 
 @app.post("/upload")
 async def upload_csv(file: UploadFile = File(...), db: Session = Depends(get_db)):
-    if file.filename.endswith('.csv'):
+    """upload csv file into db"""
+    if file.filename and file.filename.endswith(".csv"):
         content = await file.read()
 
-        for chunk in batched(csv_reader(content.decode('utf-8')), n=1000):
-            db.add_all([
-                Headline(identifier=identifier, headline=headline, name=name)
-                for identifier, headline, name in chunk
-            ])
+        for chunk in batched(csv_reader(content.decode("utf-8")), n=1000):
+            db.add_all(
+                [
+                    Headline(identifier=identifier, headline=headline, name=name)
+                    for identifier, headline, name in chunk
+                ]
+            )
             db.flush()
         db.commit()
     else:
-        raise HTTPException(status_code=400,
-                            detail="Invalid file format. Please upload a CSV file.")
+        raise HTTPException(
+            status_code=400, detail="Invalid file format. Please upload a CSV file."
+        )
 
     return RedirectResponse("/", 303)
 
 
 @app.get("/upload")
 async def upload_page(request: Request):
+    """upload page"""
     return templates.TemplateResponse("upload.html", {"request": request})
 
 
 def generate_csv_content(db: Session):
+    """streamer for downloading csv file"""
     # Write the header row
     header = ["id", "identifier", "headline", "name", "sentiment", "category"]
-    yield ','.join(header) + '\n'
+    yield ",".join(header) + "\n"
 
     # Write the data rows
     stmt = select(Headline).execution_options(yield_per=10)
     for headline in db.scalars(stmt):
-        row = [str(headline.id), headline.identifier, headline.headline, headline.name,
-               headline.sentiment, headline.category]
-        yield ','.join(row) + '\n'
+        row = [
+            str(headline.id),
+            headline.identifier,
+            headline.headline,
+            headline.name,
+            headline.sentiment,
+            headline.category,
+        ]
+        yield ",".join(map(str, row)) + "\n"
 
 
 @app.get("/download_csv", response_class=FileResponse)
 async def download_csv(db: Session = Depends(get_db)):
-    return StreamingResponse(generate_csv_content(db), media_type="text/csv", headers={
-        "Content-Disposition": "attachment;filename=headlines.csv"})
+    """download csv endpoint"""
+    return StreamingResponse(
+        generate_csv_content(db),
+        media_type="text/csv",
+        headers={"Content-Disposition": "attachment;filename=headlines.csv"},
+    )
 
 
 if __name__ == "__main__":
---
+++ tests/test_app.py
@@ -0,0 +1,142 @@
+import shutil
+
+import pytest
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine, select
+from sqlalchemy.orm import sessionmaker
+
+from label_data.main import Headline, app, get_db
+from label_data.models import Base
+
+# Setup a test database
+DB_NAME = "test.db"
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False)
+
+
+@pytest.fixture(scope="module")
+def test_db(tmp_path_factory):
+    db_path = tmp_path_factory.mktemp("data") / DB_NAME
+    db_url = f"sqlite:///{db_path}"
+    engine = create_engine(db_url)
+
+    Base.metadata.create_all(bind=engine)
+    TestingSessionLocal.configure(bind=engine)
+
+    # Insert a test headline
+    db = TestingSessionLocal()
+    db.add(
+        Headline(id=1, headline="Test headline", name="Test name", identifier="Test id")
+    )
+    db.commit()
+    db.close()
+
+    yield db_url
+
+    # Cleanup: remove the test database file
+    engine.dispose()
+    if db_path.exists():
+        db_path.unlink()
+    shutil.rmtree(db_path.parent)
+
+
+@pytest.fixture(scope="module")
+def client(test_db):
+    def override_get_db():
+        try:
+            db = TestingSessionLocal()
+            yield db
+        finally:
+            db.close()
+
+    app.dependency_overrides[get_db] = override_get_db
+    with TestClient(app) as c:
+        yield c
+
+
+def test_homepage(client):
+    response = client.get("/")
+    assert response.status_code == 200
+    assert "Test headline" in response.text
+
+
+def test_classify_headline(client):
+    response = client.post(
+        "/classify",
+        data={"headline_id": 1, "sentiment": "neutral", "category": "other"},
+        allow_redirects=False,
+    )
+    assert response.status_code == 303
+
+    # Check that the sentiment and category were updated correctly
+    db = TestingSessionLocal()
+    headline = db.scalars(select(Headline).where(Headline.id == 1).limit(1)).first()
+    assert headline.sentiment == "neutral"
+    assert headline.category == "other"
+    db.close()
+
+    response = client.get("/")
+    assert response.status_code == 200
+    assert "All headlines have been classified." in response.text
+
+
+def test_undo_endpoint(client):
+    # First, classify a headline
+    response = client.post(
+        "/classify",
+        data={"headline_id": 1, "sentiment": "positive", "category": "ads"},
+        allow_redirects=False,
+    )
+    assert response.status_code == 303
+
+    # Then, undo the classification
+    response = client.post("/undo/1", allow_redirects=False)
+    assert response.status_code == 303
+
+    # Verify that the sentiment and category for the headline are back to None
+    db = TestingSessionLocal()
+    headline = db.scalars(select(Headline).where(Headline.id == 1).limit(1)).first()
+    assert headline.sentiment is None
+    assert headline.category is None
+    db.close()
+
+
+def test_csv_upload(client):
+    csv_data = (
+        "id,identifier,headline,name\n" "2,Another id,Another headline,Another name\n"
+    )
+    response = client.post(
+        "/upload", files={"file": ("test.csv", csv_data, "text/csv")}
+    )
+    assert response.status_code == 200
+
+    # Check that the new headline was inserted into the database
+    db = TestingSessionLocal()
+    headline = db.scalars(select(Headline).where(Headline.id == 2).limit(1)).first()
+    assert headline.headline == "Another headline"
+    assert headline.name == "Another name"
+    assert headline.identifier == "Another id"
+    db.close()
+
+
+def test_download_csv(client):
+    response = client.get("/download_csv")
+    assert response.status_code == 200
+    assert response.headers["content-type"] == "text/csv; charset=utf-8"
+    # Check that the downloaded CSV contains the correct data
+    assert "Test headline" in response.text
+    assert "Another headline" in response.text
+
+
+def test_finished(client):
+    client.post(
+        "/classify",
+        data={"headline_id": 1, "sentiment": "positive", "category": "ads"},
+    )
+    response = client.post(
+        "/classify",
+        data={"headline_id": 2, "sentiment": "positive", "category": "ads"},
+    )
+    assert response.status_code == 200
+    assert "All headlines have been classified." in response.text
+    assert "Test headline" in response.text
+    assert "Another headline" in response.text
